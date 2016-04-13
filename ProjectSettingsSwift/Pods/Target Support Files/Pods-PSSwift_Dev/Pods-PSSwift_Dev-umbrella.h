f8ba2156989ea2de931e33349d951945151f2a2c
SoaringDragon909@gmail.com <SoaringDragon909@gmail.com>
2016-04-13 21:51:59 +0800

    添加cocoapods

---
+++ ProjectSettingsSwift/Pods/Target Support Files/Pods-PSSwift_Dev/Pods-PSSwift_Dev-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PSSwift_DevVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PSSwift_DevVersionString[];
+
---
+++ ProjectSettingsSwift/Pods/Target Support Files/Pods-ProjectSettingsSwift/Pods-ProjectSettingsSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProjectSettingsSwift : NSObject
+@end
+@implementation PodsDummy_Pods_ProjectSettingsSwift
+@end
